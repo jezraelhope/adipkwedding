a55ac395912f80cc80b094fa5f9d2645dcd6f68e
jezraelhope <jezraelhope@gmail.com>
2024-04-30 16:15:35 -0500

    rsvp and registry initial

--- src/Views/RSVP.jsx
+++ src/Views/RSVP.jsx
@@ -1,12 +1,21 @@
 const Rsvp = () => {
   return (
-    <div className="views px-3 py-16 mt-16">
-      <h1>RSVP to Our Wedding</h1>
-      <p>
-        Click the link below to visit theknot website to rsvp if you have not
-        already
-      </p>
-      <button>RSVP</button>
+    <div className="views px-3 py-16 mt-16 text-center">
+      <img src="./src/assets/ornate.webp" alt="" className="h-24 mx-auto" />
+      <h1 className="text-banner font-adipk -mt-20">rsvp</h1>
+      <div>
+        <h2 className="text-h4">Please RSVP to our Wedding</h2>
+        <p className="my-10">
+          Click the link below to visit theknot website to rsvp if you have not
+          already
+        </p>
+      </div>
+      <a
+        href="https://www.theknot.com/us/aditya-timmaraju-and-prateema-karande-aug-2024/rsvp"
+        className="underline"
+      >
+        RSVP
+      </a>
     </div>
   );
 };
--- src/Views/Registry.jsx
+++ src/Views/Registry.jsx
@@ -1,14 +1,21 @@
 const Registry = () => {
   return (
-    <div className="views px-3 py-16 mt-16">
-      <h1>Our Registry</h1>
-      <p>
-        We would like to thank you for sharing in our wedding celebration. We
-        are grateful for your presence.
-      </p>
-      <div className="buttons">
-        <button>The Knot</button>
-        <button>Amazon</button>
+    <div className="views px-3 py-16 mt-16 text-center">
+      <img src="./src/assets/ornate.webp" alt="" className="h-24 mx-auto" />
+      <h1 className="text-banner font-adipk -mt-20">registry</h1>
+      <div>
+        <p className="my-10">
+          We would like to thank you for sharing in our wedding celebration. We
+          are grateful for your presence.
+        </p>
+        <div className="buttons my-10">
+          <a href="" className="mx-3 underline">
+            The Knot
+          </a>
+          <a href="" className="mx-3 underline">
+            Amazon
+          </a>
+        </div>
       </div>
     </div>
   );
